b9b4dfe94801fbe2438ca33f5bd009600b362f22
rtsui <ric.tsui@harmonicinc.com>
2023-03-08 15:02:42 +0800

    trying split objects manually

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"io"
 	"log"
@@ -52,62 +53,109 @@ func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	// Make a reader to get incoming data.
 
-	objs := reader_to_json(conn)
+	objs := read_stream(conn)
 	log.Println("Received unserialized content:\n", objs)
 }
 
 func Uint16(b []byte) uint16 {
 	// big endian
-	result:= uint16(b[1]) | uint16(b[0])<<8
+	result := uint16(b[1]) | uint16(b[0])<<8
 	log.Println("Uint16:", result)
 	return result
 }
 
-func reader_to_json(conn net.Conn) string {
+// read from start, until magic footer is found
+func read_until_footer(r *bufio.Reader) ([]byte, error) {
+	magic_header := []byte{0xac, 0xed, 0x00, 0x05}
+	var buf bytes.Buffer
 
-	var json_str_array []string
-	r := bufio.NewReader(conn)
-
+	is_object_footer := false
 	for {
-		sop := jserial.NewSerializedObjectParser(r)
-		if t, err := r.Peek(16); err != nil {
-			log.Println("Error peeking reader:", err)
-			log.Println("Peeked:", t)
+		b, err := r.Peek(1)
+		if err != nil {
 			if err == io.EOF {
-				if len(t) < 1 {
-					break
-				} else {
-					r.ReadBytes(2) // read Uint16
-				}
+				break
 			}
+			log.Println("Error peeking:", err)
+			break
+		}
+		if b[0] == 0x70 {
+			if err != nil {
+				log.Println("Error peeking:", err)
+				break
+			}
+		} else {
+			continue
 		}
 
-		obj_arr, err := sop.ParseSerializedObjectMinimal()
+		b, err = r.Peek(3)
+		if err != nil {
+			log.Println("Error peeking 3bytes:", err)
+		}
+		// read 3 bytes
+		if b[1] == 0x78 && b[2] == 0x79 {
+			r.Discard(3)
+			is_object_footer = true
+		}
+
+		if is_object_footer {
+			buf.Write(magic_header)
+			// obj_stream:= r.Sc()
+			// buf.Write
+		}
+	}
+	return buf.Bytes(), nil
+}
+
+func read_stream(conn net.Conn) string {
+
+	r := bufio.NewReader(conn)
+
+	// split the stream into object_streams
+	// read until encountering 70 78 79
+	// then split the stream into object_streams
+	var java_object_streams [][]byte
+
+	// while not EOF, write splitted streams to java_object_streams
+	for {
+		obj_bytes, _ := read_until_footer(r)
+
+		java_object_streams = append(java_object_streams, obj_bytes)
+	}
+
+	var deserialized_objects []interface{}
+
+	for _, buf := range java_object_streams {
+		obj_arr, err := jserial.ParseSerializedObjectMinimal(buf)
+		// workaround: ALWAYS being parsed as list of one object
 		if err != nil {
 			if err == io.EOF && obj_arr == nil {
 				continue
 			}
 			if strings.Contains(err.Error(), "parsing Reset") {
-				if obj_arr == nil {
-					continue
-				} else {
-					// nothing
-				}
+				// ignore error
 			} else {
 				log.Println("Error parsing object:", obj_arr, err)
+				continue
 			}
-
 		}
+		obj := obj_arr[0]
+		deserialized_objects = append(deserialized_objects, obj)
+	}
 
-		for _, obj := range obj_arr {
-			json_obj, err := json.Marshal(obj)
-			json_str_array = append(json_str_array, string(json_obj))
-			if err != nil {
-				log.Println("Error marshalling object:", err)
-				break
-			}
-		} // end for obj_arr
-	} // end for sop parse
+	var json_str_array []string
+	for _, obj := range deserialized_objects {
+		json_str_array = append(json_str_array, to_json(obj))
+	}
 
 	return strings.Join(json_str_array, ",")
+}
+
+func to_json(t interface{}) string {
+	json_obj, err := json.Marshal(t)
+	if err != nil {
+		log.Println("Error marshalling object:", err)
+		return ""
+	}
+	return string(json_obj)
 }
