9f5bc705e37ea5ef2bde059ef68388989e2e93bc
ric-mb <null>
2023-03-11 19:43:26 +0800

    updated sample

--- reference/log4jtest/sender.java
+++ reference/log4jtest/sender.java
@@ -1,15 +1,13 @@
 import org.apache.log4j.Logger;
-
-import java.io.*;
-import java.util.*;
+import java.util.stream.IntStream;
 
 public class sender {
 
    /* Get actual class name to be printed on */
    static Logger log = Logger.getLogger(sender.class.getName());
 
-   public static void main(String[] args)throws IOException {
-      log.debug("Hello");
-      log.debug("Hello2");      log.debug("Hello3");
+   public static void main(String[] args) {
+      IntStream.range(0,5).forEach(i -> log.info("Hello " + i));
+      log.debug("Bye");
    }
 }
