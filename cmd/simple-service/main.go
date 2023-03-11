1006da2b9d06d04ee42dc06664ce1c6b93e53339
ric-mb <null>
2023-03-11 19:46:45 +0800

    fixed split stream weird behavior

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -52,6 +52,7 @@ func main() {
 }
 
 func handleRequest(conn net.Conn) {
+
 	defer conn.Close()
 	// Make a reader to get incoming data.
 
@@ -61,13 +62,25 @@ func handleRequest(conn net.Conn) {
 
 // read from start, until magic footer is found
 func split_stream(sr *bufio.Reader) ([][]byte, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered in split_stream", r)
+		}
+	}()
+
 	stream_magic_header := []byte{0xac, 0xed, 0x00, 0x05}
-	object_magic_footer := []byte{0x70, 0x78, 0x79}
-	object_magic_header := []byte{0x73, 0x72, 0x00, 0x21}
+	object_footer := []byte{0x70, 0x78, 0x79}
+	object_header := []byte{0x73, 0x72, 0x00, 0x21}
+	// 0x73 0x72 0x00 is the header for object
+	// 0x21 is the len of object FQCN "org.apache.log4j.spi.LoggingEvent"
+
+	// inaccurate interpretation, but should work for now
+	// actual specification mentioned in https://xz.aliyun.com/t/3847
 
 	object_streams := [][]byte{}
 
-	// first 4 bytes should be magic header
+	// first 2 bytes should be magic value 0xaced, second 2 bytes should be protocol version
+	// hardcode to 0x0005
 	if h, _ := sr.Peek(4); !bytes.Equal(h, stream_magic_header) {
 		return nil,
 			errors.New("Error reading magic header, got this instead: " + string(h))
@@ -80,14 +93,13 @@ func split_stream(sr *bufio.Reader) ([][]byte, error) {
 			break
 		}
 
-		is_object_footer := false
+		// is_object_footer := false
 		has_err := false
-		buf := bytes.Buffer{}
-		buf.Write(stream_magic_header)
-
+		obj_write_buf := bytes.Buffer{}
+		obj_write_buf.Write(stream_magic_header)
 
 		for { // object: read bytes until magic footer is found
-			byte_peek, err := sr.Peek(1)
+			byte_peek, err := sr.ReadByte()
 			if err != nil {
 				if err == io.EOF {
 					break
@@ -96,43 +108,45 @@ func split_stream(sr *bufio.Reader) ([][]byte, error) {
 				has_err = true
 				break
 			}
-			if byte_peek[0] == 0x70 { // set is_object_footer
-				b, err := sr.Peek(7)
+			if byte_peek == 0x70 { // set is_object_footer
+				byte_peek_2, err := sr.Peek(2)
 				if err != nil {
-					// log.Println("Error peeking 7bytes:", err, len(b))
-					if len(b) < 3 {
-						log.Println("Error peeking 3bytes:", err, len(b))
-						has_err = true
-						break
-					}
-					// else it's just EOF
-				}
-				// read 7 bytes
-				// if found object footer
-				// and if next 4 bytes are 0x73 0x72 0x00 0x21 or EOF
-				// then it is really object footer
-				if bytes.Equal(b[1:3], object_magic_footer[1:]) {
-					if len(b) < 7 || bytes.Equal(b[4:], object_magic_header[1:]) {
-						sr.Discard(3)
-						is_object_footer = true
+					has_err = true
+					if err == io.EOF {
+						log.Panicln("Unexpected EOF while peeking for object footer")
+					} else {
+						log.Panicln("Error peeking for object footer:", err)
 					}
 				}
-			}
+				// expect 0x78 0x79
+				if bytes.Equal(byte_peek_2, object_footer[1:]) {
+					obj_write_buf.Write(byte_peek_2)
+					sr.Discard(2)
 
-			if is_object_footer {
-				buf.Write(object_magic_footer)
-				break
+					// readahead to confirm next object header
+					next_bytes, err := sr.Peek(4)
+					if err == io.EOF || bytes.Equal(next_bytes, object_header[:]) {
+						// is_object_footer = true
+						break // end of current object
+					} else {
+						continue
+					}
+				} else {
+					obj_write_buf.WriteByte(byte_peek)
+					obj_write_buf.Write(byte_peek_2)
+					sr.Discard(2)
+					continue // continue reading
+				}
 			}
-
-			// not object footer, write 1 byte
-			buf.WriteByte(byte_peek[0])
-			sr.Discard(1)
 
 			if has_err {
 				break
 			}
+			// not object footer, write 1 byte
+			obj_write_buf.WriteByte(byte_peek)
+
 		} // end of current object
-		object_streams = append(object_streams, buf.Bytes())
+		object_streams = append(object_streams, obj_write_buf.Bytes())
 	} // end of stream
 	return object_streams, nil
 }
@@ -152,12 +166,7 @@ func read_stream(conn net.Conn) []string {
 	if err != nil {
 		log.Println("Error splitting stream:", err)
 	}
-
-	// TODO; testing
-	// for _, stream := range java_object_streams {
-	// 	log.Println("Received serialized content:\n", stream)
-	// }
-	// TODO end testing
+	log.Println("Splitted #", len(java_object_streams))
 
 	var deserialized_objects []interface{}
 
